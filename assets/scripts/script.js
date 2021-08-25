b8f34a5b0abd9f6ceb64d4b564e3dde39b3f2a6c
Matt Luna <mat.c.luna@gmail.com>
2021-08-24 23:13:01 -0500

    Fixed the main icon and the formatting on bootstrap

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,3 @@
-var getWeather = function () {
-  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=dallas,texas&units=imperial&appid=cc7ed6f786635293096d197e16858884`;
-  console.log(apiUrl);
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-        console.log(data.main.feels_like);
-      });
-    } else {
-      alert("Error: IEX quote is not found");
-    }
-  })
-    .catch(function (error) {
-      //notice this catch getting changed out to the end of the .then()
-      alert("Unable to connect to IEX");
-    });
-};
-
-getWeather();
-
 var weatherboardDates = function () {
 
   var curDate = moment().format('L');
@@ -65,6 +44,10 @@ var mainWeather = function (city, state) {
               var uvIndex = data.current.uvi;
               console.log(uvIndex);
               $(".uvIndex").empty().append(uvIndex);
+              var mwicon = data.current.weather[0].icon;
+              var iconApi = `http://openweathermap.org/img/wn/${mwicon}@2x.png`;
+              console.log(iconApi);
+              $("#mwicon").attr('src', iconApi);
             })
           }
         })
@@ -78,8 +61,6 @@ var mainWeather = function (city, state) {
       alert("Unable to connect to IEX");
     });
 };
-
-mainWeather("Dallas", "Texas");
 
 var calledWeather = function (city, state) {
   var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${state}&units=imperial&appid=cc7ed6f786635293096d197e16858884`;
@@ -114,16 +95,34 @@ var calledWeather = function (city, state) {
         };
       });
     } else {
-      alert("Error: IEX quote is not found");
+      alert("Error: Open Weather is not found");
     }
   })
     .catch(function (error) {
       //notice this catch getting changed out to the end of the .then()
-      alert("Unable to connect to IEX");
+      alert("Unable to connect to Open Weather");
     });
 };
 
-calledWeather("Dallas", "Texas");
+var searchHistory = localStorage.getItem('#searchCity');
+console.log(searchHistory);
+
+var defOrLocal = function(){
+  console.log(searchHistory);
+  if (searchHistory){
+    var city = searchHistory.split(', ');
+    document.getElementById("searchCity").innerHTML = city[0];
+    mainWeather(city[0], city[1]);
+    calledWeather(...city);
+    console.log("Search History Exists");
+  }else {
+    mainWeather("Dallas", "Texas");
+    calledWeather("Dallas", "Texas");
+    document.getElementById("searchCity").innerHTML = "Dallas";
+  }
+};
+
+defOrLocal();
 
 // $(document).click("#searchSubmit", function() {
 //   var searchIn = $("#searchCityState").val();
@@ -134,15 +133,20 @@ calledWeather("Dallas", "Texas");
 //   document.getElementsByClassName("searchCity").innerHTML = "Houston";
 // });
 
-var searchSubmit = function () {
-  document.getElementById("searchCity").innerHTML = "Houston";
-  console.log("Houston");
-};
 
 // $(document).click("#searchSubmit", function() {
 //   document.getElementById("searchCity").innerHTML = "Houston";
 //   console.log("Houston");
 // });
+
+var arr = []
+
+var places = function () {
+  arr.push('city');
+}
+
+places()
+  localStorage.setItem('history', arr);
 
 $("#searchSubmit").click(function () {
   var searchIn = $("#searchCityState").val();
@@ -156,43 +160,38 @@ $("#searchSubmit").click(function () {
   calledWeather(city[0], city[1]);
 });
 
-$("#btnDallas").click(function () {
-  document.getElementById("searchCity").innerHTML = "Dallas";
-  mainWeather("Dallas", "Texas");
-  calledWeather("Dallas", "Texas");
-});
 
-$("#btnElPaso").click(function () {
-  document.getElementById("searchCity").innerHTML = "El Paso";
-  mainWeather("El Paso", "Texas");
-  calledWeather("El Paso", "Texas");
-});
 
-$("#btnAustin").click(function () {
-  document.getElementById("searchCity").innerHTML = "Austin";
-  mainWeather("Austin", "Texas");
-  calledWeather("Austin", "Texas");
-});
 
-$("#btnMarfa").click(function () {
-  document.getElementById("searchCity").innerHTML = "Marfa";
-  mainWeather("Marfa", "Texas");
-  calledWeather("Marfa", "Texas");
-});
 
-$("#btnCorpus").click(function () {
-  document.getElementById("searchCity").innerHTML = "Corpus";
-  mainWeather("Corpus Christi", "Texas");
-  calledWeather("Corpus Christi", "Texas");
-});
 
-//next steps - create the logic to split the city from the state - done
-//There is a problem with the click function - it is not clearing
-  //If there is a comma and a space, find the space, and delete it and pass the remainder to the city
-  //How do I clear out the prior information? 
-  //There is error handling on the api call which will reject it if it is not valid.
-//Pass the city and the state into the api - Done
-//Need to clean-up and display more information on the 5 day forecast
-//Make the links clickable - still left to do 
-  //to make them clickable, I guess an event listener for these specific locations? (hardcoded to these locations - this shouldn't take too long, then turn it in.
-  //It won't have the local storage, but it is good enough for now. 
+
+// $("#btnDallas").click(function () {
+//   document.getElementById("searchCity").innerHTML = "Dallas";
+//   mainWeather("Dallas", "Texas");
+//   calledWeather("Dallas", "Texas");
+// });
+
+// $("#btnElPaso").click(function () {
+//   document.getElementById("searchCity").innerHTML = "El Paso";
+//   mainWeather("El Paso", "Texas");
+//   calledWeather("El Paso", "Texas");
+// });
+
+// $("#btnAustin").click(function () {
+//   document.getElementById("searchCity").innerHTML = "Austin";
+//   mainWeather("Austin", "Texas");
+//   calledWeather("Austin", "Texas");
+// });
+
+// $("#btnMarfa").click(function () {
+//   document.getElementById("searchCity").innerHTML = "Marfa";
+//   mainWeather("Marfa", "Texas");
+//   calledWeather("Marfa", "Texas");
+// });
+
+// $("#btnCorpus").click(function () {
+//   document.getElementById("searchCity").innerHTML = "Corpus";
+//   mainWeather("Corpus Christi", "Texas");
+//   calledWeather("Corpus Christi", "Texas");
+// });
